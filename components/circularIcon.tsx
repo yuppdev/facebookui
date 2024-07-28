edbca5d850dc2a6cd1877615889ba4d407e27a05
yuppdev <yupp.dev@gmail.com>
2024-07-28 22:44:59 +0700

    first commit

---
+++ components/circularIcon.tsx
@@ -0,0 +1,13 @@
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const circularIcon = () => {
+  return (
+    <View>
+      <Text>circularIcon</Text>
+    </View>
+  )
+}
+
+export default circularIcon
